22299bdeb3ad5d8963a91dda2f563c877706167b
Sumit Chourasia <sumitchourasia91@gmail.com>
2014-07-29 10:49:55 +0000

    user template page created.

---
+++ M2E/App/Pages/UserAfterLogin/Controller/userAfterLoginApp.js
@@ -0,0 +1,107 @@
+'use strict';
+define([appLocation.userPostLogin], function (app) {
+    app.config(function ($routeProvider) {
+
+        $routeProvider.when("/", { templateUrl: "../../App/Pages/UserAfterLogin/Index/Index.html" }).
+                       when("/edit", { templateUrl: "../../App/Pages/UserAfterLogin/EditPage/EditPage.html" }).
+                       when("/showTemplate", { templateUrl: "../../App/Pages/UserAfterLogin/ShowTemplate/ShowTemplate.html" }).
+                       when("/editTemplate/:username/:templateid", { templateUrl: "../../App/Pages/UserAfterLogin/EditTemplate/EditTemplate.html" }).
+                       when("/templateSample/:type/:subType", { templateUrl: "../../App/Pages/UserAfterLogin/TemplateSample/TemplateSample.html" }).
+                       otherwise({ templateUrl: "../../Resource/templates/beforeLogin/contentView/404.html" });
+
+    });
+
+    app.run(function ($rootScope, $location, CookieUtil, SessionManagementUtil) { //Insert in the function definition the dependencies you need.
+
+        $rootScope.$on("$locationChangeStart", function (event, next, current) {
+
+            var headerSessionData = {
+                UTMZT: CookieUtil.getUTMZT(),
+                UTMZK: CookieUtil.getUTMZK(),
+                UTMZV: CookieUtil.getUTMZV()
+            }
+
+            SessionManagementUtil.isValidSession(headerSessionData);
+
+            gaWeb("BeforeLogin-Page Visited", "Page Visited", next);
+            var path = next.split('#');
+            gaPageView(path, 'title');
+        });
+    });
+
+    app.controller('UserAfterMasterPage', function ($scope, $http, $rootScope, CookieUtil) {
+
+        $scope.ClientCategoryList = [
+       {
+           MainCategory: "Category",
+           subCategoryList: [
+           {
+               value: "Data entry", dropDownMenuShow: true, dropDownSubMenuClass: "dropdown-submenu", dropDownMenuClass: "dropdown-menu", dropDownSubMenuArrow: "dropdown", dropDownMenuList: [
+               { value: "Verification & Duplication", link: "#/VerificationAndDuplicationSample" },
+               { value: "Data Entry", link: "#" },
+               { value: "Search the web", link: "#" },
+               { value: "Do Excel work", link: "#" },
+               { value: "Find information", link: "#" },
+               { value: "Post advertisements", link: "#" },
+               { value: "Transcription", link: "#" }
+               ]
+           },
+           {
+               value: "Content Writing", dropDownMenuShow: true, dropDownSubMenuClass: "dropdown-submenu", dropDownMenuClass: "dropdown-menu", dropDownSubMenuArrow: "dropdown", dropDownMenuList: [
+                 { value: "Article writing", link: "#" },
+                 { value: "Blog writing", link: "#" },
+                 { value: "Copy typing", link: "#" },
+                 { value: "Powerpoint", link: "#" },
+                 { value: "Short stories", link: "#" },
+                 { value: "Travel writing", link: "#" },
+                 { value: "Reviews", link: "#" },
+                 { value: "Product descriptions", link: "#" }
+               ]
+           },
+           {
+               value: "Survey", dropDownMenuShow: true, dropDownSubMenuClass: "dropdown-submenu", dropDownMenuClass: "dropdown-menu", dropDownSubMenuArrow: "dropdown", dropDownMenuList: [
+                 { value: "Product survey", link: "#/templateSample/survey/productSurvey" },
+                 { value: "User feedback survey", link: "#" },
+                 { value: "Pools", link: "#" }
+               ]
+           },
+           {
+               value: "Moderation", dropDownMenuShow: true, dropDownSubMenuClass: "dropdown-submenu", dropDownMenuClass: "dropdown-menu", dropDownSubMenuArrow: "dropdown", dropDownMenuList: [
+                 { value: "Moderating Ads", link: "#" },
+                 { value: "Moderating Photos", link: "#" },
+                 { value: "Moderating Music", link: "#" },
+                 { value: "Moderating Video", link: "#" }
+               ]
+           },
+           {
+               value: "Ads", dropDownMenuShow: true, dropDownSubMenuClass: "dropdown-submenu", dropDownMenuClass: "dropdown-menu", dropDownSubMenuArrow: "dropdown", dropDownMenuList: [
+                 { value: "Facebook Views", link: "#" },
+                 { value: "Facebook likes", link: "#" },
+                 { value: "Video reviewing", link: "#" },
+                 { value: "Comments on social media", link: "#" }
+               ]
+           }
+           ]
+       }
+        ];
+    });
+
+
+    function loadjscssfile(filename, filetype) {
+        var fileref = "";
+        if (filetype == "js") { //if filename is a external JavaScript file
+            fileref = document.createElement('script');
+            fileref.setAttribute("type", "text/javascript");
+            fileref.setAttribute("src", filename);
+        }
+        else if (filetype == "css") { //if filename is an external CSS file
+            fileref = document.createElement("link");
+            fileref.setAttribute("rel", "stylesheet");
+            fileref.setAttribute("type", "text/css");
+            fileref.setAttribute("href", filename);
+        }
+        if (typeof fileref != "undefined")
+            document.getElementsByTagName("head")[0].appendChild(fileref);
+    }
+
+});
---
+++ M2E/App/Pages/UserAfterLogin/Index/index.js
@@ -0,0 +1,80 @@
+'use strict';
+define([appLocation.userPostLogin], function (app) {
+
+    app.controller('UserAfterLoginIndex', function ($scope, $http, $route, $rootScope, CookieUtil) {
+        $scope.logoImage = { url: logoImage };
+        $('title').html("index"); //TODO: change the title so cann't be tracked in log
+
+        //$scope.InProgressTaskList = [{ showEllipse: true, title: "my first template", timeShowType: "info", showTime: "5 hours", editId: "", creationDate: "an 2014" },
+        //    { showEllipse: true, title: "my second template", timeShowType: "danger", showTime: "2 hours", editId: "", creationDate: "feb 2013" },
+        //    { showEllipse: true, title: "my third template", timeShowType: "warning", showTime: "1 day", editId: "", creationDate: "march 3023" },
+        //    { showEllipse: true, title: "my fourth template", timeShowType: "success", showTime: "3 days", editId: "", creationDate: "aug 1203" },
+        //    { showEllipse: true, title: "my fifth template", timeShowType: "default", showTime: "5 hours", editId: "", creationDate: "nov 2015" }
+        //];
+        var url = ServerContextPah + '/Client/GetAllTemplateInformation';
+        var headers = {
+            'Content-Type': 'application/json',
+            'UTMZT': CookieUtil.getUTMZT(),
+            'UTMZK': CookieUtil.getUTMZK(),
+            'UTMZV': CookieUtil.getUTMZV()
+        };
+        startBlockUI('wait..', 3);
+        $http({
+            url: url,
+            method: "POST",
+            data: "",
+            headers: headers
+        }).success(function (data, status, headers, config) {
+            //$scope.persons = data; // assign  $scope.persons here as promise is resolved here
+            stopBlockUI();
+            if (data.Status == "200") {
+                $scope.InProgressTaskList = data.Payload;
+            }
+
+        }).error(function (data, status, headers, config) {
+
+        });
+
+        $scope.openTemplateEditPageWithId = function (id) {
+            //$('#closeModalPopup' + id).click();
+            //alert(id);
+            location.href = "#/editTemplate/edit/" + id;
+        }
+
+        $scope.deleteTemplateEditPageWithId = function (id) {
+            $('#closeModalPopup' + id).click();
+            var url = ServerContextPah + '/Client/DeleteTemplateDetailById?username=' + userSession.username + '&id=' + id;
+            if (confirm("Template will be permanently deleted. Are you sure?") == true) {
+                startBlockUI('wait..', 3);
+                $http({
+                    url: url,
+                    method: "POST",
+                    headers: { 'Content-Type': 'application/json' }
+                }).success(function (data, status, headers, config) {
+                    //$scope.persons = data; // assign  $scope.persons here as promise is resolved here
+
+                    if (data.Status == "200") {
+                        stopBlockUI();
+                        showToastMessage("Success", "Deleted Successfully");
+                        $route.reload();
+                    }
+                    else if (data.Status == "404") {
+                        stopBlockUI();
+                        alert("This template is not present in database");
+                    }
+                    else if (data.Status == "500") {
+                        stopBlockUI();
+                        alert("Internal Server Error Occured");
+                    }
+                }).error(function (data, status, headers, config) {
+
+                });
+            } else {
+
+            }
+
+
+        }
+    });
+
+});
