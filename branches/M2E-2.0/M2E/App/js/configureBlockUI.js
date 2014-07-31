9db4afe25b9c03d16091391fd8872407272aacce
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-07-31 10:02:08 +0000

    wysihtml5 fancy box implementation

--- branches/M2E-2.0/M2E/App/js/configureBlockUI.js
+++ branches/M2E-2.0/M2E/App/js/configureBlockUI.js
@@ -1,6 +1,7 @@
 ﻿function startBlockUI(mssg, size) {
     $.blockUI({
         message: '<h' + size + '><img src="../../App/img/loading/loading123.gif" />' + mssg + '</h' + size + '>',
+        baseZ: 2000,
         css: {
             border: 'none',
             padding: '15px',
