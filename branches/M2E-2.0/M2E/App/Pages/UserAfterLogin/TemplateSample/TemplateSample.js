4b510948b982252c6678978044d981c0465b55d2
dgchanchal <dgchanchal@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-08-06 17:38:36 +0000

    About Us modified

---
+++ branches/M2E-2.0/M2E/App/Pages/UserAfterLogin/TemplateSample/TemplateSample.js
@@ -0,0 +1,13 @@
+'use strict';
+define([appLocation.userPostLogin], function (app) {
+
+    app.controller('UserAfterLoginEditPage', function ($scope, $http, $rootScope, CookieUtil) {
+        $('title').html("edit page"); //TODO: change the title so cann't be tracked in log
+        $("input[type='checkbox'], input[type='radio']").iCheck({
+            checkboxClass: 'icheckbox_minimal',
+            radioClass: 'iradio_minimal'
+        });
+
+    });
+
+});
