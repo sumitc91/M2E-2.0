61b5354850f3821863d36e0fbb13e82d5809cd29
Sumit Chourasia <sumitchourasia91@gmail.com>
2014-08-07 10:07:52 +0000

    signalr push notification implemented.

--- M2E/App/Pages/UserAfterLogin/TemplateSample/TemplateSample.js
+++ M2E/App/Pages/UserAfterLogin/TemplateSample/TemplateSample.js
@@ -1,13 +1,14 @@
 'use strict';
 define([appLocation.userPostLogin], function (app) {
 
-    app.controller('UserAfterLoginEditPage', function ($scope, $http, $rootScope, CookieUtil) {
+    app.controller('UserAfterLoginTemplateSample', function ($scope, $http, $rootScope, CookieUtil) {
         $('title').html("edit page"); //TODO: change the title so cann't be tracked in log
+
         $("input[type='checkbox'], input[type='radio']").iCheck({
             checkboxClass: 'icheckbox_minimal',
             radioClass: 'iradio_minimal'
         });
-
+        
     });
 
 });
--- M2E/Global.asax.cs
+++ M2E/Global.asax.cs
@@ -14,6 +14,7 @@ namespace M2E
     {
         protected void Application_Start()
         {
+            RouteTable.Routes.MapHubs();
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
