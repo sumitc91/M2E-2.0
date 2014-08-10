439b8598bf3e19439bcbd258dff99bb191485dab
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-08-10 14:07:04 +0000

    real time before login data implementation.

---
+++ branches/M2E-2.0/M2E/Models/DataResponse/BeforeLoginUserProjectDetailsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace M2E.Models.DataResponse
+{
+    public class BeforeLoginUserProjectDetailsModel
+    {
+        public string TotalProjects { get; set; }
+        public string SuccessRate { get; set; }
+        public string TotalUsers { get; set; }
+        public string ProjectCategories { get; set; }
+    }
+}
