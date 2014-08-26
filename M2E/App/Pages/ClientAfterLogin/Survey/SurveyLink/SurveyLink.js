00fd5a1e4dfd7657e25664614e37d11a833b2afd
Sumit Chourasia <sumitchourasia91@gmail.com>
2014-08-26 12:08:56 +0000

    pages created for client view.

---
+++ M2E/App/Pages/ClientAfterLogin/Survey/SurveyLink/SurveyLink.js
@@ -0,0 +1,11 @@
+'use strict';
+define([appLocation.postLogin], function (app) {
+
+    //getting user info..
+    app.controller('ClientAfterLoginSurveyLinkTemplate', function ($scope, $http, $rootScope, $routeParams, CookieUtil) {
+        //alert("create product controller");    
+        
+    });
+
+});
+
