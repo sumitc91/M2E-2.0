0f8c81653ba6f5bcce669dca3be29b81178572e3
Sumit Chourasia <sumitchourasia91@gmail.com>
2014-08-28 13:32:23 +0000

    facebook service

--- M2E/Controllers/SocialAuthController.cs
+++ M2E/Controllers/SocialAuthController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
+using M2E.Models;
+using zestork.Service;
+using Facebook;
 
 namespace M2E.Controllers
 {
@@ -19,14 +23,44 @@ namespace M2E.Controllers
         public JsonResult Login(string type)
         {
             
+
             return Json("test",JsonRequestBehavior.AllowGet);
         }
 
-        [HttpPost]
         public JsonResult FBLogin(string type)
         {
+            var response = new ResponseModel<string>();
 
-            return Json("");
+            String code = Request.QueryString["code"];
+            string app_id = string.Empty;
+            string app_secret = string.Empty;
+            string returnUrl = "http://localhost:8111/SocialAuth/FBLogin/facebook/";
+            app_id = ConfigurationManager.AppSettings["FacebookAppID"].ToString();
+            app_secret = ConfigurationManager.AppSettings["FacebookAppSecret"].ToString();
+
+            string scope = "";
+            if (code == null)
+            {
+                response.Status = 199;
+                response.Message = "reload page with given url";
+                response.Payload = (string.Format(
+                    "https://graph.facebook.com/oauth/authorize?client_id={0}&redirect_uri={1}&scope={2}",
+                    app_id, returnUrl, scope));
+                
+                return Json(response,JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                string access_token = new FacebookService().getFacebookAuthToken(returnUrl, scope, code, app_id, app_secret);
+                var fb = new FacebookClient(access_token);
+                //var fb = new FacebookClient();
+                dynamic result = fb.Get("fql",
+                    new { q = "SELECT page_id FROM page_fan WHERE uid=100001648098091 AND page_id=223215721036909" });
+
+            }
+            return Json("", JsonRequestBehavior.AllowGet);
         }
+
+        
     }
 }
--- M2E/Service/SocialNetwork/facebook/FacebookService.cs
+++ M2E/Service/SocialNetwork/facebook/FacebookService.cs
@@ -13,155 +13,55 @@ namespace zestork.Service
 {
     public class FacebookService
     {
-        //private ILogger logger = new Logger(Convert.ToString(MethodBase.GetCurrentMethod().DeclaringType));
+        public string getFacebookAuthToken(string returnUrl, string scope, string code, string app_id, string app_secret)
+        {
 
-        //public LogOnModel Login(string returnUrl, string code, string referral,string userType)
-        //{
-        //    var userData = new LogOnModel();
-        //    userData = checkFacebookAuthorization(returnUrl, code, userType);
-        //    return userData;
-        //}
+            Dictionary<string, string> tokens = new Dictionary<string, string>();
+            string url = string.Format(
+                "https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&scope={2}&code={3}&client_secret={4}",
+                app_id, returnUrl, scope, code, app_secret);
 
-        //private LogOnModel checkFacebookAuthorization(string returnUrl, string code, string userType)
-        //{
-        //    var userData = new LogOnModel();
-        //    try
-        //    {
-        //        var _db = new ZestorkContainer();
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
 
-        //        string app_id = string.Empty;
-        //        string app_secret = string.Empty;
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
 
-        //        app_id = ConfigurationManager.AppSettings["FacebookAppID"].ToString();
-        //        app_secret = ConfigurationManager.AppSettings["FacebookAppSecret"].ToString();
-                                              
-        //        string scope = "";
-        //        if (code == null)
-        //        {
-        //            userData.ReturnUrl = (string.Format(
-        //                "https://graph.facebook.com/oauth/authorize?client_id={0}&redirect_uri={1}&scope={2}",
-        //                app_id, returnUrl, scope));
+                StreamReader reader = new StreamReader(response.GetResponseStream());
 
-        //            return userData;
-        //        }
-        //        else
-        //        {
+                string vals = reader.ReadToEnd();
 
-        //            string access_token = getFacebookAuthToken(returnUrl, scope, code,app_id,app_secret);
-        //            var client = new FacebookClient(access_token);
-        //            dynamic me = client.Get("me");
-        //            String userName = Convert.ToString(me.username);
-        //            userData.User = new User();
-        //            if (_db.Users.Any(x => x.Username == userName + "@facebook.com"))
-        //            {
-        //                Users user = _db.Users.SingleOrDefault(x => x.Username == userName + "@facebook.com");
-        //                //user is already registered
-        //                userData.User.FirstName = user.FirstName;
-        //                userData.User.LastName = user.LastName;
-        //                userData.User.Username = user.Username;
-        //                userData.User.Gender = user.gender;
-        //                userData.User.ImageUrl = user.ImageUrl;
-        //                userData.statusCode = "200";
-        //            }
-        //            else
-        //            {
-        //                // add user in database
-        //                String ID = Guid.NewGuid().ToString();
-        //                String ImageUrl = FacebookService.GetPictureUrl(userName);                        
-        //                userData.User.FirstName = me.first_name;
-        //                userData.User.LastName = me.last_name;
-        //                userData.User.Username = me.username;                        
-        //                userData.User.ImageUrl = ImageUrl;
-        //                userData.User.Username = userName + "@facebook.com";
-        //                var user = new Users
-        //                {
-        //                    Username = userName + "@facebook.com",
-        //                    Password = Guid.NewGuid().ToString(),
-        //                    Source = "facebook",
-        //                    isActive = "true",
-        //                    Type = userType!=null?userType:"NA",
-        //                    guid = Guid.NewGuid().ToString(),
-        //                    FirstName = me.first_name,
-        //                    LastName = me.last_name,
-        //                    gender = "NA",
-        //                    ImageUrl = ImageUrl
-        //                };
+                foreach (string token in vals.Split('&'))
+                {
+                    tokens.Add(token.Substring(0, token.IndexOf("=")),
+                        token.Substring(token.IndexOf("=") + 1, token.Length - token.IndexOf("=") - 1));
+                }
 
-        //                _db.Users.Add(user);
+            }
 
-        //                try
-        //                {
-        //                    _db.SaveChanges();
-        //                    userData.statusCode = "200";
-        //                }
-        //                catch (DbEntityValidationException e)
-        //                {
-        //                    dbContextException dbContextException = new CommonMethods.dbContextException();
-        //                    dbContextException.logDbContextException(e);
-        //                    throw;
-        //                }
-        //            }
+            string access_token = tokens["access_token"];
+            return access_token;
+        }
 
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        logger.Error("facebook login error", ex);
-        //        throw;
-        //    }
-            
-
-        //    return userData;
-        //}
-
-        //private string getFacebookAuthToken(string returnUrl, string scope, string code, string app_id, string app_secret)
-        //{
-            
-        //    Dictionary<string, string> tokens = new Dictionary<string, string>();
-        //    string url = string.Format(
-        //        "https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&scope={2}&code={3}&client_secret={4}",
-        //        app_id, returnUrl, scope, code, app_secret);
-
-        //    HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
-
-        //    using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
-        //    {
-
-        //        StreamReader reader = new StreamReader(response.GetResponseStream());
-
-        //        string vals = reader.ReadToEnd();
-
-        //        foreach (string token in vals.Split('&'))
-        //        {
-        //            tokens.Add(token.Substring(0, token.IndexOf("=")),
-        //                token.Substring(token.IndexOf("=") + 1, token.Length - token.IndexOf("=") - 1));
-        //        }
-
-        //    }
-
-        //    string access_token = tokens["access_token"];
-        //    return access_token;
-        //}
-        //public static string GetPictureUrl(string faceBookId)
-        //{
-        //    WebResponse response = null;
-        //    string pictureUrl = string.Empty;
-        //    try
-        //    {
-        //        WebRequest request = WebRequest.Create(string.Format("http://graph.facebook.com/{0}/picture?type=large", faceBookId));
-        //        response = request.GetResponse();
-        //        pictureUrl = response.ResponseUri.ToString();
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        //? handle
-        //    }
-        //    finally
-        //    {
-        //        if (response != null) response.Close();
-        //    }
-        //    return pictureUrl;
-        //}
+        public static string GetPictureUrl(string faceBookId)
+        {
+            WebResponse response = null;
+            string pictureUrl = string.Empty;
+            try
+            {
+                WebRequest request = WebRequest.Create(string.Format("http://graph.facebook.com/{0}/picture?type=large", faceBookId));
+                response = request.GetResponse();
+                pictureUrl = response.ResponseUri.ToString();
+            }
+            catch (Exception ex)
+            {
+                //? handle
+            }
+            finally
+            {
+                if (response != null) response.Close();
+            }
+            return pictureUrl;
+        }
 
     }
 }
