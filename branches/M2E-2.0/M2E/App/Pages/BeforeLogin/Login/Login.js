a182cbed52dedaa59faf4bb823c00082cdf05203
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-09-01 14:05:16 +0000

    facebook mapping implemented.

--- branches/M2E-2.0/M2E/App/Pages/BeforeLogin/Login/Login.js
+++ branches/M2E-2.0/M2E/App/Pages/BeforeLogin/Login/Login.js
@@ -146,6 +146,23 @@ define([appLocation.preLogin], function (app) {
             }
 
         }
+
+        $scope.openFacebookAuthWindow = function () {
+            var win = window.open("/SocialAuth/FBLogin/facebook", "Ratting", "width=800,height=480,0,status=0,scrollbars=1");
+            win.onunload = onun;
+
+            function onun() {
+                if (win.location != "about:blank") // This is so that the function 
+                // doesn't do anything when the 
+                // window is first opened.
+                {
+                    //$route.reload();
+                    location.reload();
+                    //alert("closed");
+                }
+            }
+        }
+
     });
 
     function isValidEmailAddress(emailAddress) {
