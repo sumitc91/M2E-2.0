0bea183be0e55d38cdc6bcb7194850eb9fe63473
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-09-02 14:23:05 +0000

    linkedin login and registration service implemented.

--- branches/M2E-2.0/M2E/App/Pages/BeforeLogin/Login/Login.js
+++ branches/M2E-2.0/M2E/App/Pages/BeforeLogin/Login/Login.js
@@ -173,6 +173,23 @@ define([appLocation.preLogin], function (app) {
             }
         }
 
+        $scope.openLinkedinAuthWindow = function () {
+            var win = window.open("/SocialAuth/LinkedinLogin", "Ratting", "width=" + popWindow.width + ",height=" + popWindow.height + ",0,status=0,scrollbars=1");
+            win.onunload = onun;
+
+            function onun() {
+                if (win.location != "about:blank") // This is so that the function 
+                // doesn't do anything when the 
+                // window is first opened.
+                {
+                    //$route.reload();
+                    //alert("working");
+                    //location.reload();
+                    //alert("closed");
+                }
+            }
+        }
+
         $('.TextBoxBeforeLoginFormSubmitButtonClass').keypress(function (e) {
             if (e.keyCode == 13)
                 $('#LoginFormSubmitButtonId').click();
---
+++ branches/M2E-2.0/M2E/Service/SocialNetwork/linkedin/linkedinService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace M2E.Service.SocialNetwork.linkedin
+{
+    public class linkedinService
+    {
+        
+    }
+}
