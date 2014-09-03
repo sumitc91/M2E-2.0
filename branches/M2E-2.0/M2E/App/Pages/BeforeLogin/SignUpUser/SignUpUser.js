1b5c05866c29d0a3d6da925abe69528e364ea80c
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-09-03 12:14:15 +0000

    google login implementation.

--- branches/M2E-2.0/M2E/App/Pages/BeforeLogin/SignUpUser/SignUpUser.js
+++ branches/M2E-2.0/M2E/App/Pages/BeforeLogin/SignUpUser/SignUpUser.js
@@ -119,6 +119,56 @@ define([appLocation.preLogin], function (app) {
 
         }
 
+        $scope.openFacebookAuthWindow = function () {
+            var win = window.open("/SocialAuth/FBLogin/facebook", "Ratting", "width=" + popWindow.width + ",height=" + popWindow.height + ",0,status=0,scrollbars=1");
+            win.onunload = onun;
+
+            function onun() {
+                if (win.location != "about:blank") // This is so that the function 
+                // doesn't do anything when the 
+                // window is first opened.
+                {
+                    //$route.reload();
+                    //alert("working");
+                    //location.reload();
+                    //alert("closed");
+                }
+            }
+        }
+
+        $scope.openLinkedinAuthWindow = function () {
+            var win = window.open("/SocialAuth/LinkedinLogin", "Ratting", "width=" + popWindow.width + ",height=" + popWindow.height + ",0,status=0,scrollbars=1");
+            win.onunload = onun;
+
+            function onun() {
+                if (win.location != "about:blank") // This is so that the function 
+                // doesn't do anything when the 
+                // window is first opened.
+                {
+                    //$route.reload();
+                    //alert("working");
+                    //location.reload();
+                    //alert("closed");
+                }
+            }
+        }
+
+        $scope.openGoogleAuthWindow = function () {
+            var win = window.open("/SocialAuth/GoogleLogin/", "Ratting", "width=" + popWindow.width + ",height=" + popWindow.height + ",0,status=0,scrollbars=1");
+            win.onunload = onun;
+
+            function onun() {
+                if (win.location != "about:blank") // This is so that the function 
+                // doesn't do anything when the 
+                // window is first opened.
+                {
+                    //$route.reload();
+                    //alert("working");
+                    //location.reload();
+                    //alert("closed");
+                }
+            }
+        }
 
     });
 
