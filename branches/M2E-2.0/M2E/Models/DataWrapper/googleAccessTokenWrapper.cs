3678f71af0dffbf8c3be518204535062b9a03b21
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-09-03 09:35:14 +0000

    google login implementation.

---
+++ branches/M2E-2.0/M2E/Models/DataWrapper/googleAccessTokenWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace M2E.Models.DataWrapper
+{
+    public class googleAccessTokenWrapper
+    {
+        public string access_token { get; set; }
+    }
+}
---
+++ branches/M2E-2.0/M2E/Service/SocialNetwork/google/googleService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Net;
+using M2E.Models.DataWrapper;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace M2E.Service.SocialNetwork.google
+{
+    public class googleService
+    {
+        
+    }
+}
