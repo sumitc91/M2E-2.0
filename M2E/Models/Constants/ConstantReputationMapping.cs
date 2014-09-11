670ad83b138e6f72823fdc8b0f0528a32643790b
Sumit Chourasia <sumitchourasia91@gmail.com>
2014-09-11 11:10:08 +0000

    user reputation.

---
+++ M2E/Models/Constants/ConstantReputationMapping.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace M2E.Models.Constants
+{
+    public class ConstantReputationMapping
+    {
+        public const string FirstUserRecommendation = "1"; 
+    }
+}
