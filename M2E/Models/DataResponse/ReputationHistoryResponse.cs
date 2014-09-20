aae1054dfc60a57520bf0ed5634d939b3090be19
Sumit Chourasia <sumitchourasia91@gmail.com>
2014-09-20 11:06:59 +0000

    user reputation update.

---
+++ M2E/Models/DataResponse/ReputationHistoryResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace M2E.Models.DataResponse
+{
+    public class ReputationHistoryResponse
+    {
+        public string dateTime { get; set; }
+        public string description { get; set; }
+        public string type { get; set; }
+        public string subType { get; set; }
+        public string username { get; set; }
+        public string reputation { get; set; }        
+    }
+}
