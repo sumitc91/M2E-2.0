8c5849c87d2112f22e25746fc381d78d8fae54e7
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2015-01-08 11:54:28 +0000

    M2E changes for mobile facebook login implementation.

--- branches/M2E-2.0/M2E/Controllers/SocialAuthController.cs
+++ branches/M2E-2.0/M2E/Controllers/SocialAuthController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Configuration;
 using M2E.Models;
+using M2E.Service;
 using M2E.Service.Referral;
 using M2E.Service.UserService;
 using zestork.Service;
@@ -51,6 +52,24 @@ namespace M2E.Controllers
             return Json("test",JsonRequestBehavior.AllowGet);
         }
 
+        public JsonResult FacebookLoginMobileApi()
+        {
+            var response = new ResponseModel<LoginResponse>();
+            String fid = Request.QueryString["fid"];
+            String access_token = Request.QueryString["accessToken"];            
+            try
+            {                
+                response = new SocialAuthService().CheckAndSaveFacebookUserInfoIntoDatabase(fid, Constants.NA,access_token,true);                
+            }
+            catch (DbEntityValidationException e)
+            {
+                DbContextException.LogDbContextException(e);
+                response.Status = 500;
+                response.Message = "Failed";
+            }
+
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
         public JsonResult userMapping()
         {
             var response = new ResponseModel<LoginResponse>();
@@ -82,135 +101,8 @@ namespace M2E.Controllers
             }
             else
             {
-                var ifFacebookUserAlreadyRegistered = _db.FacebookAuths.SingleOrDefault(x => x.facebookId == fid);
-                if (ifFacebookUserAlreadyRegistered.username != Constants.NA)
-                {
-                    if (_db.Users.Any(x => x.Username == ifFacebookUserAlreadyRegistered.username))
-                    {
-                        var user = _db.Users.SingleOrDefault(x => x.Username == ifFacebookUserAlreadyRegistered.username);
-                        if (user != null)
-                        {
-                            var data = new Dictionary<string, string>();
-                            data["Username"] = user.Username;
-                            data["Password"] = user.Password;
-                            data["userGuid"] = user.guid;
-
-                            var encryptedData = EncryptionClass.encryptUserDetails(data);                           
-                            
-                            response.Payload = new LoginResponse();
-                            response.Payload.UTMZK = encryptedData["UTMZK"];
-                            response.Payload.UTMZV = encryptedData["UTMZV"];
-                            response.Payload.TimeStamp = DateTime.Now.ToString(CultureInfo.InvariantCulture);
-                            response.Payload.Code = "210";
-                            response.Status = 210;
-                            response.Message = "user Login via facebook";
-                            try
-                            {
-                                user.KeepMeSignedIn = "true";//keepMeSignedIn.Equals("true", StringComparison.OrdinalIgnoreCase) ? "true" : "false";
-                                user.Locked = Constants.status_false;
-                                _db.SaveChanges();
-
-                                var session = new M2ESession(ifFacebookUserAlreadyRegistered.username);
-                                TokenManager.CreateSession(session);
-                                response.Payload.UTMZT = session.SessionId;
-                                return Json(response, JsonRequestBehavior.AllowGet);
-                                
-                            }
-                            catch (DbEntityValidationException e)
-                            {
-                                DbContextException.LogDbContextException(e);
-                                response.Payload.Code = "500";
-                                
-                                return Json(response, JsonRequestBehavior.AllowGet);
-                            }
-                        }
-                        else
-                            response.Payload.Code = "403";
-                    }
-                }
-                else
-                {
-                    //save user details in database ..
-
-                    var fb = new FacebookClient(ifFacebookUserAlreadyRegistered.AuthToken);
-                    dynamic result = fb.Get("fql",
-                                new { q = "SELECT uid, first_name, last_name, sex, pic_big_with_logo, username FROM user WHERE uid=me()" });
-                    
-                    var guid = Guid.NewGuid().ToString();
-                    var user = new User
-                    {
-                        Username = result.data[0].username+"@facebook.com",
-                        Password = EncryptionClass.Md5Hash(Guid.NewGuid().ToString()),
-                        Source = "facebook",
-                        isActive = "true",
-                        Type = "user",
-                        guid = Guid.NewGuid().ToString(),
-                        fixedGuid = Guid.NewGuid().ToString(),
-                        FirstName = result.data[0].first_name,
-                        LastName = result.data[0].last_name,
-                        gender = result.data[0].sex,
-                        ImageUrl = result.data[0].pic_big_with_logo
-                    };
-                    _db.Users.Add(user);
-
-                    if (!Constants.NA.Equals(refKey))
-                    {
-                        new ReferralService().payReferralBonusAsync(refKey, user.Username,Constants.status_true);
-                    }
-                                                           
-                    try
-                    {
-                        ifFacebookUserAlreadyRegistered.username = user.Username;
-                        _db.SaveChanges();
-                        
-                        var data = new Dictionary<string, string>();
-                        data["Username"] = user.Username;
-                        data["Password"] = user.Password;
-                        data["userGuid"] = user.guid;
-
-                        var encryptedData = EncryptionClass.encryptUserDetails(data);                        
-
-                        response.Payload = new LoginResponse();
-                        response.Payload.UTMZK = encryptedData["UTMZK"];
-                        response.Payload.UTMZV = encryptedData["UTMZV"];
-                        response.Payload.TimeStamp = DateTime.Now.ToString(CultureInfo.InvariantCulture);
-                        response.Payload.Code = "210";
-                        response.Status = 210;
-                        response.Message = "user Login via facebook";
-                        try
-                        {                           
-                            var session = new M2ESession(ifFacebookUserAlreadyRegistered.username);
-                            TokenManager.CreateSession(session);
-                            response.Payload.UTMZT = session.SessionId;                            
-                        }
-                        catch (DbEntityValidationException e)
-                        {
-                            DbContextException.LogDbContextException(e);
-                            response.Status = 500;
-                            response.Message = "Internal Server Error !!";
-                        }
-                        var signalRHub = new SignalRHub();
-                        string totalProjects = "";
-                        string successRate = "";
-                        string totalUsers = _db.Users.Count().ToString(CultureInfo.InvariantCulture);
-                        string projectCategories = "";
-
-                        new UserMessageService().SendUserNotificationForAccountVerificationSuccess(
-                            user.Username, user.Type
-                        );
-
-                        var hubContext = GlobalHost.ConnectionManager.GetHubContext<SignalRHub>();
-                        hubContext.Clients.All.updateBeforeLoginUserProjectDetails(totalProjects, successRate, totalUsers, projectCategories);                        
-                    }
-                    catch (DbEntityValidationException e)
-                    {
-                        DbContextException.LogDbContextException(e);
-                        response.Status = 500;
-                        response.Message = "Internal Server Error !!!";                        
-                    }
-
-                }
-                
+                //TODO:need to call method socialauthservice
+                response = new SocialAuthService().CheckAndSaveFacebookUserInfoIntoDatabase(fid, refKey,Constants.NA,false);
             }
             
             return Json(response, JsonRequestBehavior.AllowGet);
