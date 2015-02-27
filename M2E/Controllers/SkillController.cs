ddd553c91c8ab2ee7dff40a177c591900772a234
Sumit Chourasia <sumitchourasia91@gmail.com>
2015-02-27 11:45:35 +0000

    skill test page

---
+++ M2E/Controllers/SkillController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace M2E.Controllers
+{
+    public class SkillController : Controller
+    {
+        //
+        // GET: /Skill/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
